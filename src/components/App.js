94e458f8b77d95c2c91b4beaaae937f0f5a26dfb
HmChef25 <daniel.l.meadows@gmail.com>
2025-07-27 17:22:34 -0400

    feat: initial setup with pricing gallery and vercel files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="gallery">
+      <img src="assets/images/20250402_100311.jpg" alt="Flavor Track 1" />
+      <img src="assets/images/20250408_173643.jpg" alt="Flavor Track 2" />
+      <img src="assets/images/20250514_124512.jpg" alt="Flavor Track 3" />
+    </div>
+  );
+}
+
+export default App;
